3175ea4e92ef2a18ff53565bd353d10367e4b734
Crista Cooper <aine3d@gmail.com>
2017-11-17 19:21:24 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,12 +3,12 @@
 katz_deli = []
 
 
-def line(katz_deli)
+def currentLine(line)
   katz_deli = []
 
 
 
-  if katz_deli.length == 0
+  if line.length == 0
     puts "The line is currently empty."
   else katz_deli.length > 0
     puts "Welcome #{katz_deli[index]}.  You are number #{katz_deli[index.with_index]} in line."
