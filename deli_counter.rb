ca778a7fdb798b21d09b5217385120216b98a467
Crista Cooper <aine3d@gmail.com>
2017-11-17 19:14:28 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,7 +11,7 @@ def line(katz_deli)
   if currentLine == 0
     puts "The line is currently empty."
   else currentLine > 0
-    puts "The line is currently #{katz_deli.with_index + 1}. #{name}."
+    puts "The line is currently #{katz_deli[index +1]}. #{name}."
 
   end
 end
