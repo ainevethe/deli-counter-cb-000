84158f461986e98f172ccea80cdbd0d987712d13
Crista Cooper <aine3d@gmail.com>
2017-11-17 19:26:39 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,13 +5,14 @@ katz_deli = []
 def line(array)
   x = 0
   message = "The line is currently: "
-  
+
   if line.length == 0
     puts "The line is currently empty."
   elsif katz_deli.length >= 1
     array.each_with_index |name, index|
     message += "#{index + 1}. #{name} "
   end
+  puts message
 end
 
 def take_a_number(array, name)
