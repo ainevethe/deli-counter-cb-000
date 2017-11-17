6137170236e37db81ef315bffd104c40adb2ca3e
Crista Cooper <aine3d@gmail.com>
2017-11-17 19:03:49 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+
+
+katz_deli = []
+
+
+def line(name)
+
+  line = katz_deli.size
+
+  if line > 0
+    puts "The line is currently #{katz_deli[index + 1]}. #{name}."
+
+  end
+end
+
+
+def take_a_number(katz_deli, name)
+
+
+end
+
+def now_serving
+
+
+end
