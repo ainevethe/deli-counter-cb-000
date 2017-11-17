d622a6df8bc260c442349cfe2146391d7aebdfb9
Crista Cooper <aine3d@gmail.com>
2017-11-17 19:31:46 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,13 +4,13 @@ katz_deli = []
 
 def line(lineup)
   x = 0
-  message = "The line is currently: "
+  message = "The line is currently:"
 
   if lineup.length == 0
     puts "The line is currently empty."
   elsif lineup.length >= 1
     lineup.each_with_index do |name, index|
-    message += "#{index + 1}. #{name}"
+    message += " #{index + 1}. #{name}"
   end
   puts message
 end
